44dddf6f2add2da782bc9f20829c27e1309094b9
koczurekk <koczurekk@gmail.com>
2017-03-18 15:33:11 +0100

    Moved sfgf::Plane methods implementation outside of the class

--- SFGF/Plane.hpp
+++ SFGF/Plane.hpp
@@ -8,34 +8,42 @@ namespace sfgf {
 	class Plane
 		: public Polygon {
 	public:
-		Plane()
-			: Polygon(4)
-		{}
+		Plane();
 
-		void setTexture(const sf::Texture& tex) {
-			Polygon::setTexture(tex, {
-				{0.f, 0.f},
-				{static_cast<float>(tex.getSize().x), 0.f},
-				static_cast<sf::Vector2f>(tex.getSize()),
-				{0.f, static_cast<float>(tex.getSize().y)}
-			});
-		}
+		void setTexture(const sf::Texture& tex);
 
-		void setSize(sf::Vector2f size) {
-			setVertices({
-				{0, 0},
-				{size.x, 0},
-				size,
-				{0, size.y}
-			});
-		}
-		sf::Vector2f getSize() {
-			return getVertices()[2].position;
-		}
-		void selfCenter() {
-			setOrigin(getSize() / 2.f);
-		}
+		void setSize(sf::Vector2f size);
+		sf::Vector2f getSize();
+		void selfCenter();
 	};
+
+	Plane::Plane()
+		: Polygon(4)
+	{}
+
+	void Plane::setTexture(const sf::Texture& tex) {
+		Polygon::setTexture(tex, {
+			{0.f, 0.f},
+			{static_cast<float>(tex.getSize().x), 0.f},
+			static_cast<sf::Vector2f>(tex.getSize()),
+			{0.f, static_cast<float>(tex.getSize().y)}
+		});
+	}
+
+	void Plane::setSize(sf::Vector2f size) {
+		setVertices({
+			{0, 0},
+			{size.x, 0},
+			size,
+			{0, size.y}
+		});
+	}
+	sf::Vector2f Plane::getSize() {
+		return getVertices()[2].position;
+	}
+	void Plane::selfCenter() {
+		setOrigin(getSize() / 2.f);
+	}
 }
 
 #endif // PLANE_HPP
