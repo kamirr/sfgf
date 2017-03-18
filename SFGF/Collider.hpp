28ee56095fc0a69a44736a712b0079567502a33a
koczurekk <koczurekk@gmail.com>
2017-03-18 15:26:49 +0100

    Moved sfgf::Collider methods implementation outside of the class

--- SFGF/Collider.hpp
+++ SFGF/Collider.hpp
@@ -10,114 +10,123 @@ namespace sfgf {
 		std::vector<sf::Vector2f> m_arr;
 
 	public:
-		void apply_transform(const sf::Transform& t) {
-			std::transform(
-				m_arr.begin(),
-				m_arr.end(),
-				m_arr.begin(),
-				[&t](sf::Vector2f p) {
-					return t.transformPoint(p);
-				}
-			);
-		}
-		void push_back(sf::Vector2f pt) {
-			m_arr.push_back(pt);
-		}
-		void clear() {
-			m_arr.clear();
-		}
+		static bool lineIntersection(sf::Vector2f p1, sf::Vector2f q1, sf::Vector2f p2, sf::Vector2f q2);
 
-		static bool lineIntersection(sf::Vector2f p1, sf::Vector2f q1, sf::Vector2f p2, sf::Vector2f q2) {
-			auto isOnSegment = [](sf::Vector2f p, sf::Vector2f q, sf::Vector2f r) {
-				return (q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) &&
-						q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y));
-			};
-			auto orientation = [](sf::Vector2f p, sf::Vector2f q, sf::Vector2f r) {
-				int val = (q.y - p.y) * (r.x - q.x) -
-						  (q.x - p.x) * (r.y - q.y);
+		void apply_transform(const sf::Transform& t);
+		void push_back(sf::Vector2f pt);
+		void clear();
 
-				if (val == 0) {
-					return 0;
-				}
+		bool intersects(const Collider& poly);
+		bool contains(sf::Vector2f point);
+		bool collides(const Collider& poly);
+	};
 
-				return (val > 0) ? 1 : 2;
-			};
+	static bool Collider::lineIntersection(sf::Vector2f p1, sf::Vector2f q1, sf::Vector2f p2, sf::Vector2f q2)  {
+		auto isOnSegment = [](sf::Vector2f p, sf::Vector2f q, sf::Vector2f r) {
+			return (q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) &&
+					q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y));
+		};
+		auto orientation = [](sf::Vector2f p, sf::Vector2f q, sf::Vector2f r) {
+			int val = (q.y - p.y) * (r.x - q.x) -
+					  (q.x - p.x) * (r.y - q.y);
 
-			// Find the four orientations needed for general and
-			// special cases
-			int o1 = orientation(p1, q1, p2);
-			int o2 = orientation(p1, q1, q2);
-			int o3 = orientation(p2, q2, p1);
-			int o4 = orientation(p2, q2, q1);
-
-			// General case
-			if (o1 != o2 && o3 != o4)
-				return true;
-
-			// Special Cases
-			if ((o1 == 0 && isOnSegment(p1, p2, q1))
-			||  (o2 == 0 && isOnSegment(p1, q2, q1))
-			||  (o3 == 0 && isOnSegment(p2, p1, q2))
-			||  (o4 == 0 && isOnSegment(p2, q1, q2))) {
-				return true;
+			if (val == 0) {
+				return 0;
 			}
 
-			return false;
-		}
+			return (val > 0) ? 1 : 2;
+		};
 
+		// Find the four orientations needed for general and
+		// special cases
+		int o1 = orientation(p1, q1, p2);
+		int o2 = orientation(p1, q1, q2);
+		int o3 = orientation(p2, q2, p1);
+		int o4 = orientation(p2, q2, q1);
 
-		bool intersects(const Collider& poly) {
-			auto arr = m_arr;
-			arr.push_back(arr.front());
+		// General case
+		if (o1 != o2 && o3 != o4)
+			return true;
 
-			auto poly_arr = poly.m_arr;
-			poly_arr.push_back(poly_arr.front());
-
-			for(auto i = 1u; i < arr.size(); ++i) {
-				auto p1 = arr[i - 1], q1 = arr[i];
-
-				for(auto j = 1u; j < poly_arr.size(); ++j) {
-					auto p2 = poly_arr[j - 1], q2 = poly_arr[j];
-
-					if(lineIntersection(p1, q1, p2, q2)) {
-						return true;
-					}
-				}
-			}
-
-			return false;
-		}
-		bool contains(sf::Vector2f point) {
-			int i, j, nvert = m_arr.size();
-			bool c = false;
-
-			for(i = 0, j = nvert - 1; i < nvert; j = i++) {
-				auto pti = m_arr[i];
-				auto ptj = m_arr[j];
-
-				if(((pti.y >= point.y) != (ptj.y >= point.y)) &&
-					(point.x <= (ptj.x - pti.x) * (point.y - pti.y) / (ptj.y - pti.y) + pti.x)) {
-					c = !c;
-				}
-			  }
-
-			  return c;
-		}
-
-		bool collides(const Collider& poly) {
-			if(intersects(poly)) {
-				return true;
-			}
-
-			for(auto v: poly.m_arr) {
-				if(!contains(v)) {
-					return false;
-				}
-			}
-
+		// Special Cases
+		if ((o1 == 0 && isOnSegment(p1, p2, q1))
+		||  (o2 == 0 && isOnSegment(p1, q2, q1))
+		||  (o3 == 0 && isOnSegment(p2, p1, q2))
+		||  (o4 == 0 && isOnSegment(p2, q1, q2))) {
 			return true;
 		}
-	};
+
+		return false;
+	}
+
+	void Collider::apply_transform(const sf::Transform& t) {
+		std::transform(
+			m_arr.begin(),
+			m_arr.end(),
+			m_arr.begin(),
+			[&t](sf::Vector2f p) {
+				return t.transformPoint(p);
+			}
+		);
+	}
+	void Collider::push_back(sf::Vector2f pt) {
+		m_arr.push_back(pt);
+	}
+	void Collider::clear() {
+		m_arr.clear();
+	}
+
+	bool Collider::intersects(const Collider& poly) {
+		auto arr = m_arr;
+		arr.push_back(arr.front());
+
+		auto poly_arr = poly.m_arr;
+		poly_arr.push_back(poly_arr.front());
+
+		for(auto i = 1u; i < arr.size(); ++i) {
+			auto p1 = arr[i - 1], q1 = arr[i];
+
+			for(auto j = 1u; j < poly_arr.size(); ++j) {
+				auto p2 = poly_arr[j - 1], q2 = poly_arr[j];
+
+				if(lineIntersection(p1, q1, p2, q2)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+	bool Collider::contains(sf::Vector2f point) {
+		int i, j, nvert = m_arr.size();
+		bool c = false;
+
+		for(i = 0, j = nvert - 1; i < nvert; j = i++) {
+			auto pti = m_arr[i];
+			auto ptj = m_arr[j];
+
+			if(((pti.y >= point.y) != (ptj.y >= point.y)) &&
+				(point.x <= (ptj.x - pti.x) * (point.y - pti.y) / (ptj.y - pti.y) + pti.x)) {
+				c = !c;
+			}
+		  }
+
+		  return c;
+	}
+
+	bool Collider::collides(const Collider& poly) {
+		if(intersects(poly)) {
+			return true;
+		}
+
+		for(auto v: poly.m_arr) {
+			if(!contains(v)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
 
 #endif // COLLIDER_HPP
