c576f7f62785b20d05a4085faaddef485f6f20ec
koczurekk <koczurekk@gmail.com>
2017-03-19 07:57:47 +0100

    Code cleanup & updated example

--- SFGF/GameObject.hpp
+++ SFGF/GameObject.hpp
@@ -19,38 +19,48 @@ namespace sfgf {
 		const CopyLock m_copyLock;
 
 	public:
-		GameObject() {
-			getObjects().push_back(this);
-		}
-		virtual ~GameObject() {
-			auto& v = getObjects();
-			v.erase(std::remove(v.begin(), v.end(), this), v.end());
-		}
+		GameObject();
+		virtual ~GameObject();
 
-		static void updateAll(sf::Time t) {
-			for(auto ptr: getObjects()) {
-				ptr->update(t);
-			}
-		}
+		static void updateAll(sf::Time t);
+		virtual void update(sf::Time);
 
-		void setParent(const GameObject& parent) {
-			m_parent = &parent;
-		}
-		const GameObject& getParent() {
-			return *m_parent;
-		}
+		void setParent(const GameObject& parent);
+		const GameObject& getParent() const;
 
-		sf::Transform getTransform() const {
-			if(m_parent) {
-				return m_parent->getTransform() * sf::Transformable::getTransform();
-			}
-
-			return sf::Transformable::getTransform();
-		}
-
-		virtual void update(sf::Time)
-		{ }
+		sf::Transform getTransform() const;
 	};
+
+	GameObject::GameObject() {
+		getObjects().push_back(this);
+	}
+	GameObject::~GameObject() {
+		auto& v = getObjects();
+		v.erase(std::remove(v.begin(), v.end(), this), v.end());
+	}
+
+	void GameObject::updateAll(sf::Time t) {
+		for(auto ptr: getObjects()) {
+			ptr->update(t);
+		}
+	}
+	void GameObject::update(sf::Time)
+	{ }
+
+	void GameObject::setParent(const GameObject& parent) {
+		m_parent = &parent;
+	}
+	const GameObject& GameObject::getParent() const {
+		return *m_parent;
+	}
+
+	sf::Transform GameObject::getTransform() const {
+		if(m_parent) {
+			return m_parent->getTransform() * sf::Transformable::getTransform();
+		}
+
+		return sf::Transformable::getTransform();
+	}
 }
 
 #endif // GAMEOBJECT_HPP
--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 #include <iostream>
 
 int main() {
+	sf::Texture tx;
+	tx.loadFromFile("img.png");
+
 	sfgf::Plane p1;
 	p1.setPosition({400, 300});
 	p1.setSize({200, 200});
 	p1.selfCenter();
-
-	sf::Texture tx;
-	tx.loadFromFile("img.png");
 	p1.setTexture(tx);
 
 	sfgf::Polygon p2(3);
@@ -25,8 +25,8 @@ int main() {
 		{0, tx.getSize().y}
 	});
 
-	p1.setSampleCollider(p1.getDefaultSampleCollider());
-	p2.setSampleCollider(p2.getDefaultSampleCollider());
+	sfgf::Plane p3;
+	p3.setColor({255, 255, 255, 50});
 
 	sf::RenderWindow app({800, 600, 32}, "app");
 
@@ -35,6 +35,7 @@ int main() {
 		auto dt = timer.restart();
 		std::cout << 1 / dt.asSeconds() << std::endl;
 
+		sfgf::GameObject::updateAll(dt);
 		for(sf::Event ev; app.pollEvent(ev);) {
 			if(ev.type == sf::Event::Closed) {
 				app.close();
@@ -44,14 +45,18 @@ int main() {
 
 		app.clear({20, 20, 20});
 
-		p1.update(sf::Time::Zero);
-		p2.update(sf::Time::Zero);
+		p1.rotate(dt.asSeconds() * 120);
+
+		p3.setPosition({p1.getGlobalBounds().left, p1.getGlobalBounds().top});
+		p3.setSize({p1.getGlobalBounds().width, p1.getGlobalBounds().height});
 
 		p2.setPosition(app.mapPixelToCoords(sf::Mouse::getPosition(app)));
-		p2.setColor(p1.collides(p2) ? sf::Color{255, 0, 0} : sf::Color{255, 255, 255});
+		p1.setColor(p1.collides(p2) ? sf::Color{255, 0, 0} : sf::Color{255, 255, 255});
+		p3.setColor(p3.collides(p2) ? sf::Color{255, 0, 0, 50} : sf::Color{255, 255, 255, 50});
 
 		app.draw(p1);
 		app.draw(p2);
+		app.draw(p3);
 
 		app.display();
 	}
